89670420cfac94b421c432b7cf3f0782c4945192
Krooper <98321262+Krooper@users.noreply.github.com>
2022-07-12 23:10:31 +0300

    made a method that filles and returns initial array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+// Метод, создающий исходный массив. Кол-во эл-ов - случайное (от 1 до 10)
+// Кол-во сиволов в строке (элементе массива) - случайное (от 1 до 5)
+string[] ArrayFiller(){
+    string[] filledArray = new string[new Random().Next(1, 11)];
+    int len = filledArray.Length;
+    int strLen = new Random().Next(1, 5)
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < strLen; j++){
+            filledArray[i] += "*";
+        }
+    }
+    return filledArray;
+}
