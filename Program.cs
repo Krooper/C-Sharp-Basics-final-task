f200ad76771c1bd87174df767f2510794359f60b
Krooper <98321262+Krooper@users.noreply.github.com>
2022-07-12 23:17:47 +0300

    made a method that filteres initial array and returns a filtered one

--- Program.cs
+++ Program.cs
@@ -8,11 +8,23 @@ Clear();
 string[] ArrayFiller(){
     string[] filledArray = new string[new Random().Next(1, 11)];
     int len = filledArray.Length;
-    int strLen = new Random().Next(1, 5)
+    int strLen = new Random().Next(1, 5);
     for (int i = 0; i < len; i++){
         for (int j = 0; j < strLen; j++){
             filledArray[i] += "*";
         }
     }
     return filledArray;
+}
+
+// Метод, возвращающий новый массив, в котором длина элемента (строки) не превышает 3-х символов
+string[] ArrayElementFilter(string[] inputArray){
+    string[] filteredArray = new string[0];
+    int len = inputArray.Length;
+    for (int i = 0; i < len; i++){
+        if (inputArray[i].Length <= 3) {
+            filteredArray.Append(inputArray[i]);
+        }
+    }
+    return filteredArray;
 }
